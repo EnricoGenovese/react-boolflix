43937d54001907bc6d1e567aa0e9e93feb9125db
EnricoGenovese <enricogeno@libero.it>
2025-01-15 15:18:44 +0100

    add most popular section in page;

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,23 @@
 import { useGlobalContext } from "../contexts/GlobalContext";
 import List from "./List";
-
+import Loader from "./Loader";
 
 export default function Main() {
 
-    const { movies, tvSeries, searching } = useGlobalContext()
+    const { movies, tvSeries, popularMovies, popularSeries, searching, loading } = useGlobalContext()
     return (
         < main className="d-flex flex-column">
             {!searching ? (
-                <h2> Prova a cercare un film o una serie TV</h2>
+                <>
+                    <List section="Popular Movies" list={popularMovies} />
+                    <List section="Popular TV Series" list={popularSeries} />
+                </>
             ) : (
                 <>
                     <List section="Movies" list={movies} />
                     <List section="TV Series" list={tvSeries} />
+                    <List section="Popular Movies" list={popularMovies} />
+                    <List section="Popular TV Series" list={popularSeries} />
                 </>
             )}
         </main >
--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -9,12 +9,19 @@ const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
     const [movies, setMovies] = useState([]);
-    const [tvSeries, setTvSeries] = useState([])
+    const [tvSeries, setTvSeries] = useState([]);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [popularSeries, setPupularSeries] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [searching, setSearching] = useState(false);
 
-    const [searching, setSearching] = useState(false)
+    useEffect(() => {
+        getPopular
+    }, []);
 
     function getData(query, endpoint) {
-        axios.get(apiUrl + "/search/" + endpoint, {
+        setLoading(true)
+        axios.get(apiUrl + "search/" + endpoint, {
             params: {
                 api_key: apiKey,
                 query,
@@ -33,12 +40,37 @@ const GlobalProvider = ({ children }) => {
         })
     }
 
+    function getPopular(endpoint) {
+        axios.get(apiUrl + endpoint + "/popular", {
+            params: {
+                api_key: apiKey,
+                language: "it-IT",
+            },
+        }).then((res) => {
+            if (endpoint === "movie") {
+                // console.log(res.data)
+                setPopularMovies(res.data.results);
+            } else {
+                // console.log(res.data)
+                setPupularSeries(res.data.results);
+            }
+        }).catch((error) => {
+            console.log(error)
+        }).finally(() => {
+            console.log("Done");
+        })
+    }
+
     function search(query) {
         if (!query) {
+            getPopular("movie");
+            getPopular("tv");
             setMovies([]);
             setTvSeries([]);
+
             setSearching(false)
         } else {
+
             getData(query, "movie");
             getData(query, "tv");
             setSearching(true);
@@ -46,7 +78,7 @@ const GlobalProvider = ({ children }) => {
 
     };
 
-    const data = { movies, tvSeries, search, searching }
+    const data = { movies, tvSeries, popularMovies, popularSeries, search, searching, loading }
 
     return (
         <GlobalContext.Provider value={data}>{children}</GlobalContext.Provider>
